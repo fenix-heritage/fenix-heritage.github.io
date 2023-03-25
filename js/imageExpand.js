f5258bc0afca6d1cd1a53af99d29742a29b65fec
ItsJustMarto <tobymartin999@hotmail.com>
2023-03-25 13:18:07 +0000

    update 25.03.23

---
+++ js/imageExpand.js
@@ -0,0 +1,7 @@
+    // Image Gallery
+    function onClick(element) {
+        document.getElementById("img1").src = element.src;
+        document.getElementById("fs1").style.display = "block";
+        var captionText = document.getElementById("caption");
+        captionText.innerHTML = element.alt;
+    }
