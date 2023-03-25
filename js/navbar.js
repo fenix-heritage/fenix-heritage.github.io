a1cf75c276cc22d303a5c50149d0d01b1147dd44
ItsJustMarto <tobymartin999@hotmail.com>
2023-03-25 12:52:26 +0000

    update 25.03.23

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
     // Add solid background to Navbar on scroll
     var myNav = document.getElementById('navbar2');
     window.onscroll = function () {
-        if (document.body.scrollTop >= screen.height || document.documentElement.scrollTop >= screen.height) {
+        if (document.body.scrollTop >= screen.height - 700 || document.documentElement.scrollTop >= screen.height - 700) {
             myNav.classList.add("nav-coloured");
             myNav.classList.remove("nav-transparent");
         }
